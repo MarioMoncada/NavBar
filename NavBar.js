870c9fed5c2319fc418907271ad50be6dbf43d62
MarioMoncada <moncacol@gmail.com>
2019-06-15 10:21:42 +0300

    first commit

---
+++ NavBar.js
@@ -0,0 +1,109 @@
+import React from "react";
+import "./stylesBarra.css";
+
+class NavBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      className: "Navbar"
+    };
+  }
+
+  handleBarra = () => {
+    const barra = document.getElementById("barrapag").getBoundingClientRect();
+    const divNextBarra = document
+      .getElementById("dvbajo")
+      .getBoundingClientRect();
+
+    if (barra.top <= 0) {
+      this.setState({ className: "NavbarTopSticky" });
+    }
+    if (divNextBarra.top >= 60) {
+      this.setState({ className: "Navbar" });
+    }
+  };
+
+  componentWillMount() {
+    this.barra = window.addEventListener(
+      "scroll",
+      this.handleBarra,
+
+      false
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          className={this.state.className}
+          id="barrapag"
+          onScroll={this.handleBarra}
+        >
+          <li>HOME</li>
+          <li>PRODUCTS</li>
+          <li>CONTACT</li>
+        </div>
+        <div className="dvbajo" id="dvbajo" onScroll={this.handleBarra}>
+          <h1>Section 1 </h1>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+          <h2>Section 2</h2>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum."
+          </p>
+          <h3>Section 3 </h3>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum."
+          </p>
+          <h4>Sectio4</h4>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+        />
+      </div>
+    );
+  }
+}
+
+export default NavBar;
